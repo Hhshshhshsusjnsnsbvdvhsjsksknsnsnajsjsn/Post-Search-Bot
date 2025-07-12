c98cb6cf1d9b8b844dca1238959116b3b90c0111
𝖢𝗈𝖽𝖾𝖿𝗅𝗂𝗑 𝖡𝗈𝗍𝗌 <159244479+Codeflix-Bots@users.noreply.github.com>
2025-07-12 17:47:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ from logging.handlers import RotatingFileHandler
 API_ID       = int(os.environ.get("API_ID", "28264594"))
 API_HASH     = os.environ.get("API_HASH", "94ca8a089020a2290fd29a41f18acb94")
 BOT_TOKEN    = os.environ.get("BOT_TOKEN", "6427124758:AAG-PILGXW3q-RGlgajUNbQJr6F0pWmqStQ")
-SESSION      = os.environ.get("SESSION", "")
+SESSION      = os.environ.get("SESSION", "Yato")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://publicdb:publicdb@cluster0.aisg0rh.mongodb.net/?retryWrites=true&w=majority")
 LOG_CHANNEL  = int(os.environ.get("LOG_CHANNEL", "-1001868871195"))
 ADMIN        = int(os.environ.get("ADMIN", "6497757690"))
